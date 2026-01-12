2ff59a3549f4ec8c9834a2e217af61431a5e3396
muzi5@kakao.com <muzi5@kakao.com>
2026-01-12 11:08:55 +0900

    feat: 카테고리 사이드바 컴포넌트 추가 및 스타일 수정

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,7 +2,34 @@ import Image from "next/image";
 import { redirect } from "next/navigation";
 
 import { BaseInput, IconInput } from "@/components/shared/inputs";
-import { CategorySidebar } from "@/components/sidebar/category-sidebar";
+import {
+  CategorySidebar,
+  CategorySidebarItem,
+} from "@/components/sidebar/category-sidebar";
+import { ROUTES } from "@/lib/routes";
+
+const categories = [
+  {
+    label: "카테고리 1",
+    href: "/test",
+  },
+  {
+    label: "카테고리 2",
+    href: ROUTES.mypage.PROFILE,
+  },
+  {
+    label: "카테고리 3",
+    href: ROUTES.mypage.PROFILE,
+  },
+  {
+    label: "카테고리 4",
+    href: ROUTES.rank.ROOT,
+  },
+  {
+    label: "카테고리 5",
+    href: ROUTES.search.ROOT,
+  },
+];
 
 export default function TestPage() {
   const isDevelopment = process.env.NODE_ENV === "development";
@@ -12,51 +39,58 @@ export default function TestPage() {
   return (
     <div className="w-full grid grid-cols-12 max-w-7xl mx-auto gap-4 p-4">
       <div className="col-span-3">
-        <CategorySidebar />
+        <CategorySidebar title="랭킹별직군">
+          <CategorySidebarItem categories={categories} />
+        </CategorySidebar>
       </div>
-      <div className="inline-flex flex-col gap-2 col-span-9">
-        {/* <Button>테스트 버튼</Button> */}
-        <div className="w-[300px] bg-gray-400 p-4 flex flex-col gap-4">
-          <BaseInput
-            rounded
-            id="phone1"
-            name="phone1"
-            value="검색어를 입력하세요"
-          />
-          <BaseInput
-            id="phone2"
-            name="phone2"
-            disabled
-            type={"text"}
-            value="검색어를 입력하세요"
-          />
+      {/* eslint-disable-next-line no-constant-binary-expression */}
+      {false && (
+        <div className="inline-flex flex-col gap-2 col-span-9">
+          {/* <Button>테스트 버튼</Button> */}
+          <div className="w-[300px] bg-gray-400 p-4 flex flex-col gap-4">
+            <BaseInput
+              rounded
+              id="phone1"
+              name="phone1"
+              value="검색어를 입력하세요"
+            />
+            <BaseInput
+              id="phone2"
+              name="phone2"
+              disabled
+              type={"text"}
+              value="검색어를 입력하세요"
+            />
 
-          <BaseInput
-            rounded
-            id="phone2"
-            name="phone2"
-            inputSize="lg"
-            type={"text"}
-            value="검색어를 입력하세요"
-          />
-          <BaseInput
-            id="phone2"
-            name="phone2"
-            inputSize="lg"
-            disabled
-            type={"text"}
-            value="검색어를 입력하세요"
-          />
+            <BaseInput
+              rounded
+              id="phone2"
+              name="phone2"
+              inputSize="lg"
+              type={"text"}
+              value="검색어를 입력하세요"
+            />
+            <BaseInput
+              id="phone2"
+              name="phone2"
+              inputSize="lg"
+              disabled
+              type={"text"}
+              value="검색어를 입력하세요"
+            />
 
-          <IconInput
-            icon={<Image src="/file.svg" alt="search" width={16} height={16} />}
-            value=":hover"
-            name="search"
-          />
+            <IconInput
+              icon={
+                <Image src="/file.svg" alt="search" width={16} height={16} />
+              }
+              value=":hover"
+              name="search"
+            />
 
-          <p className="text-15 text-gray-700">검색어를 입력하세요</p>
+            <p className="text-15 text-gray-700">검색어를 입력하세요</p>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
--- components/sidebar/category-sidebar.tsx
+++ components/sidebar/category-sidebar.tsx
@@ -1,6 +1,32 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import { ROUTES } from "@/lib/routes";
+import { cn } from "@/lib/utils";
+
+const categories = [
+  {
+    label: "카테고리 1",
+    href: "/test",
+  },
+  {
+    label: "카테고리 2",
+    href: ROUTES.mypage.PROFILE,
+  },
+  {
+    label: "카테고리 3",
+    href: ROUTES.mypage.PROFILE,
+  },
+  {
+    label: "카테고리 4",
+    href: ROUTES.rank.ROOT,
+  },
+  {
+    label: "카테고리 5",
+    href: ROUTES.search.ROOT,
+  },
+];
 
 import {
   Sidebar,
@@ -9,33 +35,52 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarSeparator,
 } from "../ui/sidebar";
 
-export function CategorySidebar() {
-  const sidebarStyle =
-    "label-medium bg-white text-gray-600 rounded-[10px] h-11.5 w-full hover:text-frog-600 hover:bg-white active:text-frog-600 active:bg-grog-100";
+interface CategorySidebarProps {
+  title: string;
+  children: React.ReactElement<CategorySidebarItemProps>;
+}
+export function CategorySidebar({ title, children }: CategorySidebarProps) {
   return (
-    <div className="w-full bg-red-300">
-      <Sidebar className="w-full h-auto">
-        <SidebarContent className="bg-green-300 rounded-lg h-auto">
-          <SidebarGroupContent>
-            <SidebarMenu className="bg-blue-600">
-              <SidebarMenuItem className="bg-red-500">
-                <SidebarMenuButton className={`${sidebarStyle}`} asChild>
-                  <Link href={ROUTES.mypage.ACTIVITY} className="w-full">
-                    카테고리 1
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-              <SidebarMenuItem className="bg-red-50">
-                <SidebarMenuButton className="" asChild>
-                  <p className="w-full ">카테고리 2</p>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            </SidebarMenu>
+    <div className="w-full p-4 border border-gray-100 rounded-10">
+      <h2 className="heading-small">{title}</h2>
+
+      <SidebarSeparator className="my-3 mx-0" />
+      <Sidebar className="w-full h-auto border-r-0">
+        <SidebarContent className="rounded-lg h-auto border-0">
+          <SidebarGroupContent className="border-none bg-none">
+            <SidebarMenu className="gap-y-0">{children}</SidebarMenu>
           </SidebarGroupContent>
         </SidebarContent>
       </Sidebar>
     </div>
   );
+}
+
+interface CategorySidebarItemProps {
+  categories: Array<{ label: string; href: string }>;
+}
+export function CategorySidebarItem({ categories }: CategorySidebarItemProps) {
+  const sidebarStyle =
+    "label-medium bg-white text-gray-600 rounded-[10px] h-11.5 w-full hover:text-frog-600 hover:bg-white ";
+  const sidebarActiveStyle = "text-frog-600 bg-frog-100 transition-300";
+
+  const pathName = usePathname();
+
+  return categories.map((item) => {
+    const activeStyle = item.href.includes(pathName)
+      ? cn(sidebarStyle, sidebarActiveStyle)
+      : sidebarStyle;
+    return (
+      <SidebarMenuItem key={item.href}>
+        <SidebarMenuButton className={activeStyle} asChild>
+          <Link href={item.href} className="w-full">
+            {item.label}
+          </Link>
+        </SidebarMenuButton>
+      </SidebarMenuItem>
+    );
+  });
 }
