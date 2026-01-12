309f178b8b75f83eda4662021424fcd9759f5e8d
muzi5@kakao.com <muzi5@kakao.com>
2026-01-12 10:32:36 +0900

    style: sidebar-item 컴포넌트 상태 스타일링

--- components/sidebar/category-sidebar.tsx
+++ components/sidebar/category-sidebar.tsx
@@ -1,3 +1,7 @@
+import Link from "next/link";
+
+import { ROUTES } from "@/lib/routes";
+
 import {
   Sidebar,
   SidebarContent,
@@ -8,18 +12,24 @@ import {
 } from "../ui/sidebar";
 
 export function CategorySidebar() {
+  const sidebarStyle =
+    "label-medium bg-white text-gray-600 rounded-[10px] h-11.5 w-full hover:text-frog-600 hover:bg-white active:text-frog-600 active:bg-grog-100";
   return (
     <div className="w-full bg-red-300">
       <Sidebar className="w-full h-auto">
-        <SidebarContent className="bg-white rounded-lg h-auto">
+        <SidebarContent className="bg-green-300 rounded-lg h-auto">
           <SidebarGroupContent>
-            <SidebarMenu>
-              <SidebarMenuItem>
-                <SidebarMenuButton
-                  className="hover:bg-inherit hover:text-red-500"
-                  asChild
-                >
-                  <p className="w-full ">카테고리 1</p>
+            <SidebarMenu className="bg-blue-600">
+              <SidebarMenuItem className="bg-red-500">
+                <SidebarMenuButton className={`${sidebarStyle}`} asChild>
+                  <Link href={ROUTES.mypage.ACTIVITY} className="w-full">
+                    카테고리 1
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem className="bg-red-50">
+                <SidebarMenuButton className="" asChild>
+                  <p className="w-full ">카테고리 2</p>
                 </SidebarMenuButton>
               </SidebarMenuItem>
             </SidebarMenu>
