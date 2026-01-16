b107edac523ea0d8c78e82e90b9bc048073f9401
muzi5@kakao.com <muzi5@kakao.com>
2026-01-16 23:51:40 +0900

    chore: 코드 변경 사항 없음

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,12 +1,17 @@
-"use client";
-
 import { redirect } from "next/navigation";
+
+import { Board } from "@/components/board/board";
 
 export default function TestPage() {
   const isDevelopment = process.env.NODE_ENV === "development";
   if (!isDevelopment) {
     redirect("/");
   }
-
-  return <></>;
+  return (
+    <div className="inner w-full">
+      <div className="w-full">
+        <Board />
+      </div>
+    </div>
+  );
 }
