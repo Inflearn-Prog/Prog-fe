d6c208c274381d33c2ce68382149430059aa9b04
muzi5@kakao.com <muzi5@kakao.com>
2026-01-18 00:47:06 +0900

    refactor: TestPage 컴포넌트에서 불필요한 코드 제거

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,24 +2,11 @@
 
 import { redirect } from "next/navigation";
 
-import { toasts } from "@/components/shared/toast";
-
 export default function TestPage() {
   const isDevelopment = process.env.NODE_ENV === "development";
   if (!isDevelopment) {
     redirect("/");
   }
 
-  return (
-    <div className="w-full">
-      <button
-        onClick={() => toasts().success("게시글이 성공적으로 등록되었습니다.")}
-      >
-        게시글이 성공적으로 등록되었습니다.
-      </button>
-      <button onClick={() => toasts().success("게시글이.")}>
-        게시글이 성공적으로 등록되었습니다.
-      </button>
-    </div>
-  );
+  return <></>;
 }
