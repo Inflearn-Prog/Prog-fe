01335ea5dd731d39b5f4c3f0800c91a0a059d232
muzi5@kakao.com <muzi5@kakao.com>
2026-01-20 12:40:24 +0900

    feat: 보드 에디터 스토리 추가

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,30 +1,10 @@
-"use client";
-import "react-quill-new/dist/quill.snow.css";
-
-import { useState } from "react";
-
-import { Board, BoardViewer } from "@/components/board/board";
-import { BoardSkeleton } from "@/components/ui/skeleton";
+import { redirect } from "next/navigation";
 
 export default function TestPage() {
-  // const isDevelopment = process.env.NODE_ENV === "development";
-  // if (!isDevelopment) {
-  //   redirect("/");
-  // }
-  const [data, setData] = useState("안녕하세요 반갑습니다");
-  console.log(data);
-  return (
-    <div className="inner w-full bg-amber-100 p-4">
-      <div className="h-200">
-        <Board
-          value={data}
-          setValue={setData}
-          placeholder="Enter text here..."
-        />
-      </div>
-      <BoardViewer content={data} />
+  const isDevelopment = process.env.NODE_ENV === "development";
+  if (!isDevelopment) {
+    redirect("/");
+  }
 
-      <BoardSkeleton />
-    </div>
-  );
+  return <></>;
 }
