89d4b86def61d16d6adafcd517b23efcfc91aef5
muzi5@kakao.com <muzi5@kakao.com>
2026-02-27 15:49:05 +0900

    fix: 헤더 왼쪽 섹션에서 md:visible을 md:block으로 수정

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -62,7 +62,7 @@ export function HeaderLeftSection() {
   return (
     <div className="flex items-center gap-x-15">
       <HeaderLogo />
-      <div className="hidden md:visible">
+      <div className="hidden md:block">
         <HeaderNavigation />
       </div>
     </div>
