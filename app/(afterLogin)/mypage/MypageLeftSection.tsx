d799dfe8ffb1ca136136dc8de2368997b1318ef2
fryzke <fryekdy@gmail.com>
2026-02-27 16:35:22 +0900

    fix: 로그아웃, 회원탈퇴 실패 시 세션 삭제하지 않도록 수정

--- app/(afterLogin)/mypage/MypageLeftSection.tsx
+++ app/(afterLogin)/mypage/MypageLeftSection.tsx
@@ -32,9 +32,6 @@ export default function MypageLeftSection() {
         router.push(ROUTES.rank.ROOT);
         router.refresh();
       } catch (error) {
-        Cookies.remove("accessToken");
-        Cookies.remove("refreshToken");
-        console.error("Logout failed:", error);
         //TODO: error 컴포넌트가 생기면 사용자 피드백 주기
         alert("로그아웃 처리 중 오류가 발생했습니다.");
         //toasts.error("로그아웃 처리 중 오류가 발생했습니다.");
@@ -60,9 +57,6 @@ export default function MypageLeftSection() {
       router.push(ROUTES.rank.ROOT);
       router.refresh();
     } catch (error) {
-      Cookies.remove("accessToken");
-      Cookies.remove("refreshToken");
-      console.error("Withdrawal failed:", error);
       alert("탈퇴 처리 중 오류가 발생했습니다. 고객센터에 문의해주세요.");
       //toasts.error("탈퇴 처리 중 오류가 발생했습니다. 고객센터에 문의해주세요.");
     }
