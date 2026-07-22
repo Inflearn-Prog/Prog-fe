e1a6e5169705fe91e1e366c20092ab344746643f
CHAERIM.J <75243536+fryzke@users.noreply.github.com>
2026-07-22 16:56:21 +0900

    feat: Google Tag Manager(GTM-MD22QF5W) 설치 (#156)
    * feat: Google Tag Manager(GTM-MD22QF5W) 설치
    
    * fix: GTM 인라인 스크립트에 nonce 전달 (CSP 차단 방지)

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./styles/globals.css";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { headers } from "next/headers";
+import Script from "next/script";
 
 import { AuthWatcher } from "@/components/auth/auth-watcher";
 import { SidebarProvider } from "@/components/ui/sidebar";
@@ -40,7 +41,29 @@ export default async function RootLayout({
       nonce={nonce}
       suppressHydrationWarning
     >
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          nonce={nonce}
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MD22QF5W');`,
+          }}
+        />
+      </head>
       <body className={`${pretendard.className} antialiased`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MD22QF5W"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <AuthProvider>
           <AuthWatcher />
           <QueryProvider>
