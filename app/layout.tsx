d5d22b337ab844326b5d1e7ca295fc9f6e07e30a
fryzke <fryekdy@gmail.com>
2026-07-22 16:48:38 +0900

    fix: GTM 인라인 스크립트에 nonce 전달 (CSP 차단 방지)

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,6 +45,7 @@ export default async function RootLayout({
         <Script
           id="gtm-script"
           strategy="afterInteractive"
+          nonce={nonce}
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
