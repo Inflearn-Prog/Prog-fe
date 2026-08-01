8f81f7b900771e78c836296ce4d606aa65321aca
fryzke <fryekdy@gmail.com>
2026-08-01 14:21:10 +0900

    feat: Microsoft Clarity(xq9uail2t7) 추적 스크립트 설치

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,6 +54,18 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-MD22QF5W');`,
           }}
         />
+        <Script
+          id="ms-clarity-script"
+          strategy="afterInteractive"
+          nonce={nonce}
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window,document,"clarity","script","xq9uail2t7");`,
+          }}
+        />
       </head>
       <body className={`${pretendard.className} antialiased`}>
         <noscript>
